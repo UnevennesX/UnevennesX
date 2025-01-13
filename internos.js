cc4afec6809ce58d2cfecd4322063f3437b2933f
UnevennesX <appreciaty@gmail.com>
2025-01-13 10:10:38 -0500

    Update internos.js

--- internos.js
+++ internos.js
@@ -1,25 +1,25 @@
 function processUrl(url) {
   try {
-    const domain = new URL(url).hostname; // Obtener el dominio
-    let generatedUrl = '';
+    // Parsear la URL y obtener el dominio
+    const parsedUrl = new URL(url);
+    const domain = parsedUrl.hostname;
 
+    // Validar el dominio
     if (domain.includes('se.navigatorsurveys.com')) {
-      // Obtener los parámetros de la URL
-      const proj = new URL(url).searchParams.get('rd_proj_ud');
-      const param = new URL(url).searchParams.get('s2');
-      if (proj && param) {
-        // Construir la URL generada
-        generatedUrl = `https://www.rdsecured.com/return?inbound_code=1000&rdud=${param}&rd_proj_ud=${proj}`;
-      } else {
-        console.error('Faltan parámetros en la URL.');
-        return null;
-      }
-    } else {
-      return null; // Dominio no válido
+      // Extraer los parámetros necesarios
+      const proj = parsedUrl.searchParams.get('rd_proj_ud');
+      const param = parsedUrl.searchParams.get('s2');
+
+      // Construir la URL generada directamente
+      const generatedUrl = `https://www.rdsecured.com/return?inbound_code=1000&rdud=${s2}&rd_proj_ud=${proj}`;
+      return generatedUrl;
     }
-    return generatedUrl;
+
+    // Si el dominio no coincide, devuelve null
+    return null;
   } catch (e) {
-    console.error('Error en la URL:', e);
+    // Manejo de errores
+    console.error('Error en la URL:', e.message);
     return null;
   }
 }
