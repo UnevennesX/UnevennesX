976613e96eedb82d368d7bb39a46bdd0b0cfeae0
UnevennesX <appreciaty@gmail.com>
2025-01-13 10:38:24 -0400

    Update invitemid.js

--- invitemid.js
+++ invitemid.js
@@ -1,5 +1,18 @@
-export function processIpsos(url) {
-  const proj = new URL(url).searchParams.get('id');
-  const param = new URL(url).searchParams.get('param1');
-  return `https://redirect.mindsharesurveys.com/v1/M7FMAqNSgNBiTKRoDbiNJ4YiXPN2cBin?proj=${param}&id=${proj}&status=1`;
+  function processUrl(url) {
+  try {
+    let domain = new URL(url).hostname; // Asegúrate de declarar la variable 'domain'
+    let generatedUrl = '';
+
+    if (domain.includes('ipsosinteractive.com')) {
+      let proj = new URL(url).searchParams.get('id');
+      let param = new URL(url).searchParams.get('param1');
+      generatedUrl = `https://redirect.mindsharesurveys.com/v1/M7FMAqNSgNBiTKRoDbiNJ4YiXPN2cBin?proj=${param}&id=${proj}&status=1`;
+    } else {
+      return null; // Para los demás dominios, puedes manejar otro caso
+    }
+    return generatedUrl;
+  } catch (e) {
+    console.error('Error en la URL:', e);
+    return null;
+  }
 }
