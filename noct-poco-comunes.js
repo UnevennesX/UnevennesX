18d01a6dacb6bae1583b0696e43684d850646db8
UnevennesX <appreciaty@gmail.com>
2025-01-13 11:24:40 -0500

    Actualizar noct-poco-comunes.js

--- noct-poco-comunes.js
+++ noct-poco-comunes.js
@@ -1,21 +1,23 @@
 function processUrl(url) {
-    try {
-      let domain = new URL(url).hostname;
-      let token = '04651664-4715-4620-8f59-16a272ff3e4a'; // Token de Notch
-      let pid1 = new URL(url).searchParams.get('pid1');
-      let refid1 = new URL(url).searchParams.get('refid1');
-      let rid = new URL(url).searchParams.get('rid');
-      let generatedUrl = '';
+  try {
+    let parsedUrl = new URL(url);
+    let domain = parsedUrl.hostname;
+    let token = '04651664-4715-4620-8f59-16a272ff3e4a'; // Token de Notch
+    let pid1 = parsedUrl.searchParams.get('pid1');
+    let refid1 = parsedUrl.searchParams.get('refid1');
+    let rid = parsedUrl.searchParams.get('rid');
+    let generatedUrl = '';
 
-      if (domain.includes('ovationworldpanel.com')) {
-        // Se genera la nueva URL con los parámetros obtenidos
-        generatedUrl = `https://notch.insights.supply/cb?token=${token}&pid1=${pid1}&refid1=${refid1}&rid=${rid}`;
-      } else {
-        throw new Error('URL no válida');
-      }
-
-      return generatedUrl;
-    } catch (e) {
-      return null;
+    if (domain.includes('ovationworldpanel.com') && pid1 && refid1 && rid) {
+      // Se genera la nueva URL con los parámetros obtenidos
+      generatedUrl = `https://notch.insights.supply/cb?token=${token}&pid1=${pid1}&refid1=${refid1}&rid=${rid}`;
+    } else {
+      throw new Error('URL no válida o parámetros faltantes');
     }
+
+    return generatedUrl;
+  } catch (e) {
+    console.error(e); // Puedes agregar un mensaje de error para depurar
+    return null;
   }
+}
