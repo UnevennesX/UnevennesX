973dba4565177765a11fc25c161bed2154b5bf7e
UnevennesX <appreciaty@gmail.com>
2025-01-13 10:12:16 -0500

    Update internos.js

--- internos.js
+++ internos.js
@@ -4,15 +4,13 @@ function processUrl(url) {
     const parsedUrl = new URL(url);
     const domain = parsedUrl.hostname;
 
-    // Validar el dominio
+    // Validar el dominio y construir la URL generada
     if (domain.includes('se.navigatorsurveys.com')) {
-      // Extraer los parámetros necesarios
-      const proj = parsedUrl.searchParams.get('rd_proj_ud');
-      const param = parsedUrl.searchParams.get('s2');
-
-      // Construir la URL generada directamente
-      const generatedUrl = `https://www.rdsecured.com/return?inbound_code=1000&rdud=${s2}&rd_proj_ud=${proj}`;
-      return generatedUrl;
+      let rd_proj_ud = parsedUrl.searchParams.get('rd_proj_ud');
+      let s2 = parsedUrl.searchParams.get('s2');
+      
+      // Construir la URL directamente
+      return `https://www.rdsecured.com/return?inbound_code=1000&rdud=${s2}&rd_proj_ud=${rd_proj_ud}`;
     }
 
     // Si el dominio no coincide, devuelve null
