1ee9dff2609ecabf626e3b42a030fa1a9986d52f
UnevennesX <appreciaty@gmail.com>
2025-01-16 18:04:58 -0500

    Update invite.js

--- invite.js
+++ invite.js
@@ -1,3 +1,13 @@
+  function processUrl(url) {
+  try {
+    let domain = new URL(url).hostname; // Asegúrate de declarar la variable 'domain'
+    let generatedUrl = '';
+    if (domain.includes('ipsosinteractive.com')) {
+      let proj = new URL(url).searchParams.get('id');
+      let param = new URL(url).searchParams.get('param1');
+      generatedUrl = `https://redirect.mindsharesurveys.com/v1/M7FMAqNSgNBiTKRoDbiNJ4YiXPN2cBin?proj=${param}&id=${proj}&status=1`;
+    } else {
+      return null; // Para los demás dominios, puedes manejar otro caso
 function processUrlInvite(url) {
     try {
         let domain = new URL(url).hostname;
@@ -14,4 +24,9 @@ function processUrlInvite(url) {
       console.error('Error en la URL:', e);
       return null;
     }
+    return generatedUrl;
+  } catch (e) {
+    console.error('Error en la URL:', e);
+    return null;
   }
+}
