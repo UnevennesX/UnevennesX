17c83412e0c63a1241534d5a6957f668104e2ac2
UnevennesX <appreciaty@gmail.com>
2025-01-16 18:33:29 -0500

    Update rid-token.js

--- rid-token.js
+++ rid-token.js
@@ -1,13 +1,12 @@
-// rid-token.js
 export async function processUrl(url) {
     try{
         let domain = new URL(url).hostname;
        const token = '034287b2-1ca0-48d1-9e45-f5ca740ef529';
-      if (domain.includes('lumen-research.com')) {
-         let rid = new URL(url).searchParams.get('participant');
-         if(rid && token){
-            return { url: `https://notch.insights.supply/cb?RID=${rid}&token=${token}`, provider: 'Lumen Research' };
-          }
+        if (domain.includes('lumen-research.com')) {
+        let participant = new URL(url).searchParams.get('participant');
+         if(participant && token){
+             return { url: `https://notch.insights.supply/cb?RID=${participant}&token=${token}`, provider: 'Lumen Research' };
+            }
         }
      return null;
      } catch(e){
