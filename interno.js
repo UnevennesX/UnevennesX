549e28d868b91ce189faefcd5408eec87043ece0
UnevennesX <appreciaty@gmail.com>
2025-01-16 17:43:03 -0500

    Update interno.js

--- interno.js
+++ interno.js
@@ -1,69 +1,37 @@
-// --------------------
-// Comienza la función 'processUrl'
-// --------------------
-function processUrl(url) {
-  try {
-    let generatedUrl = null; // Inicializamos la variable para almacenar la URL generada
+function processUrlInterno(url) {
+    try {
+        let generatedUrl = null;
+        let provider = 'Interno';
 
-    // Primer bloque: Procesar URL con 'tsid'
-    if (url.includes('tsid=')) {
-      const regex = /tsid=([a-f0-9]{32})/;
-      const match = url.match(regex);
-      if (match) {
-        const tsid = match[1]; // Extraemos el 'tsid' de la URL
-        generatedUrl = `https://tssrvy.com/r/?st=1&tsid=${tsid}`; // Generamos la nueva URL
+       if (url.includes('tsid=')) {
+         const regex = /tsid=([a-f0-9]{32})/;
+         const match = url.match(regex);
+         if (match) {
+           const tsid = match[1];
+             return { url: `https://tssrvy.com/r/?st=1&tsid=${tsid}`, provider: provider };
+        }
       }
-    } 
-    // Segundo bloque: Procesar URL con parámetros de Survey
-    else if (url.includes('')) {
-      let s2 = new URL(url).searchParams.get('s2');
-      let rd_proj_ud = new URL(url).searchParams.get('rd_proj_ud');
-      let rdud = new URL(url).searchParams.get('rdud');
-      
-      // Verificar si los parámetros necesarios existen
-      if (!rd_proj_ud && !s2 && !rdud) return 'URL no válida';
+      else if (url.includes('')) {
+        let s2 = new URL(url).searchParams.get('s2');
+         let rd_proj_ud = new URL(url).searchParams.get('rd_proj_ud');
+         let rdud = new URL(url).searchParams.get('rdud');
 
-      // Generar la URL con los parámetros disponibles
-      if (s2 && rdud) {
-        generatedUrl = `https://www.rdsecured.com/return?inbound_code=1000&rdud=${rdud}&rd_proj_ud=${rd_proj_ud}`;
-      } else if (s2) {
-        generatedUrl = `https://www.rdsecured.com/return?inbound_code=1000&rdud=${s2}&rd_proj_ud=${rd_proj_ud}`;
-      } else if (rdud) {
-        generatedUrl = `https://www.rdsecured.com/return?inbound_code=1000&rdud=${rdud}&rd_proj_ud=${rd_proj_ud}`;
-      } else if (rd_proj_ud) {
-        generatedUrl = `https://www.rdsecured.com/return?inbound_code=1000&rd_proj_ud=${rd_proj_ud}`;
-      }
-    } 
-    // Tercer bloque: Procesar URL con dominio 'router.cint.com'
-    else {
-      let domain = new URL(url).hostname;
-      let arid = new URL(url).pathname.split('/')[2];
-      let rid = new URL(url).searchParams.get('RID');
+        if (!rd_proj_ud && !s2 && !rdud) return null;
 
-      // Verificar si la URL tiene el dominio esperado y los parámetros necesarios
-      if (domain.includes('router.cint.com') && arid && rid) {
-        let token = '0749a007-a1d3-48c1-8ff3-12960c555867';
-        generatedUrl = `https://notch.insights.supply/cb?token=${token}&RID=${rid}&cint_arid=${arid}`;
-      } 
-      // Si la URL es de 'router.cint.com' pero le faltan parámetros, retornamos 'URL no válida'
-      else if (domain.includes('router.cint.com')) {
-        return 'URL no válida';
-      }
+        if (s2 && rdud) {
+           return { url: `https://www.rdsecured.com/return?inbound_code=1000&rdud=${rdud}&rd_proj_ud=${rd_proj_ud}`, provider: provider };
+          } else if (s2) {
+            return { url: `https://www.rdsecured.com/return?inbound_code=1000&rdud=${s2}&rd_proj_ud=${rd_proj_ud}`, provider: provider };
+         } else if (rdud) {
+           return { url: `https://www.rdsecured.com/return?inbound_code=1000&rdud=${rdud}&rd_proj_ud=${rd_proj_ud}`, provider: provider };
+        } else if (rd_proj_ud) {
+           return { url: `https://www.rdsecured.com/return?inbound_code=1000&rd_proj_ud=${rd_proj_ud}`, provider: provider };
+        }
+       }
+    
+      return null;
+    } catch (error) {
+      console.error('Error al procesar la URL:', error);
+      return null;
     }
-
-    // Si no se generó ninguna URL válida, retornamos 'URL no válida'
-    if (!generatedUrl) {
-      return 'URL no válida';
-    }
-
-    return generatedUrl; // Retornamos la URL generada
-
-  } catch (error) {
-    // Si ocurre un error, se captura y se devuelve 'URL no válida'
-    console.error('Error al procesar la URL:', error);
-    return 'URL no válida';
   }
-}
-// --------------------
-// Termina la función 'processUrl'
-// --------------------
