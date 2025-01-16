e93fef487e53ed05ba484a3a48d9659d8dd8f587
UnevennesX <appreciaty@gmail.com>
2025-01-16 09:03:34 -0500

    Update internos.js

--- internos.js
+++ internos.js
@@ -1,76 +1,79 @@
-// Verificar si ya existe la función processUrl
-if (typeof processUrl === 'undefined') {
-  function processUrl(url) {
-    try {
-      // Crear un objeto URL para analizar la URL dada
-      const parsedUrl = new URL(url);
+// Función para procesar las URLs de Survey
+function processSurveyUrl(url) {
+  try {
+    // Crear un objeto URL para analizar la URL dada
+    const parsedUrl = new URL(url);
 
-      // Verificar si el dominio es el que nos interesa
-      if (parsedUrl.hostname.includes('se.navigatorsurveys.com')) {
-        // Obtener los parámetros 's2', 'rd_proj_ud' y 'rdud'
-        const s2 = parsedUrl.searchParams.get('s2');
-        const rd_proj_ud = parsedUrl.searchParams.get('rd_proj_ud');
-        const rdud = parsedUrl.searchParams.get('rdud'); // Buscar el parámetro rdud
+    // Verificar si el dominio es el que nos interesa
+    if (parsedUrl.hostname.includes('se.navigatorsurveys.com')) {
+      // Obtener los parámetros 's2', 'rd_proj_ud' y 'rdud'
+      const s2 = parsedUrl.searchParams.get('s2');
+      const rd_proj_ud = parsedUrl.searchParams.get('rd_proj_ud');
+      const rdud = parsedUrl.searchParams.get('rdud'); // Buscar el parámetro rdud
 
-        // Verificar que rd_proj_ud siempre esté presente
-        if (!rd_proj_ud) {
-          // Si no se encuentra rd_proj_ud, retornar null porque es obligatorio
-          return null;
-        }
-
-        // Si ambos parámetros están presentes, se usará rdud
-        if (s2 && rdud) {
-          return `https://www.rdsecured.com/return?inbound_code=1000&rdud=${rdud}&rd_proj_ud=${rd_proj_ud}`;
-        }
-
-        // Si solo s2 está presente, usarlo
-        if (s2) {
-          return `https://www.rdsecured.com/return?inbound_code=1000&rdud=${s2}&rd_proj_ud=${rd_proj_ud}`;
-        }
-
-        // Si solo rdud está presente, usarlo
-        if (rdud) {
-          return `https://www.rdsecured.com/return?inbound_code=1000&rdud=${rdud}&rd_proj_ud=${rd_proj_ud}`;
-        }
-
-        // Si no se encuentra ni s2 ni rdud, usar el valor de rd_proj_ud sin uno de los otros parámetros
-        return `https://www.rdsecured.com/return?inbound_code=1000&rd_proj_ud=${rd_proj_ud}`;
+      // Verificar que rd_proj_ud siempre esté presente
+      if (!rd_proj_ud) {
+        // Si no se encuentra rd_proj_ud, retornar null porque es obligatorio
+        return null;
       }
 
-      // Si el dominio no es el correcto, retornar null
-      return null;
-    } catch (e) {
-      // Manejo de errores: Si la URL es inválida, imprimir el error
-      console.error('Error en la URL:', e.message);
-      return null;
+      // Si ambos parámetros están presentes, se usará rdud
+      if (s2 && rdud) {
+        return `https://www.rdsecured.com/return?inbound_code=1000&rdud=${rdud}&rd_proj_ud=${rd_proj_ud}`;
+      }
+
+      // Si solo s2 está presente, usarlo
+      if (s2) {
+        return `https://www.rdsecured.com/return?inbound_code=1000&rdud=${s2}&rd_proj_ud=${rd_proj_ud}`;
+      }
+
+      // Si solo rdud está presente, usarlo
+      if (rdud) {
+        return `https://www.rdsecured.com/return?inbound_code=1000&rdud=${rdud}&rd_proj_ud=${rd_proj_ud}`;
+      }
+
+      // Si no se encuentra ni s2 ni rdud, usar el valor de rd_proj_ud sin uno de los otros parámetros
+      return `https://www.rdsecured.com/return?inbound_code=1000&rd_proj_ud=${rd_proj_ud}`;
     }
+
+    // Si el dominio no es el correcto, retornar null
+    return null;
+  } catch (e) {
+    // Manejo de errores: Si la URL es inválida, imprimir el error
+    console.error('Error en la URL:', e.message);
+    return null;
   }
 }
 
-// Verificar si ya existe la función processDecipherUrl
-if (typeof processDecipherUrl === 'undefined') {
-  function processDecipherUrl(url) {
-    try {
-      // Extraer el dominio y el parámetro tsid de la URL
-      let domain = new URL(url).hostname;
-      let tsid = new URL(url).searchParams.get('tsid'); // Extraer el valor de tsid
+// Función para procesar las URLs de Decipher
+function processDecipherUrl(url) {
+  try {
+    // Extraer el dominio y el parámetro tsid de la URL
+    let domain = new URL(url).hostname;
+    let tsid = new URL(url).searchParams.get('tsid'); // Extraer el valor de tsid
 
-      let generatedUrl = '';
+    let generatedUrl = '';
 
-      // Verificar si la URL es de decipherinc.com
-      if (domain.includes('decipherinc.com') && tsid) {
-        // Generar la nueva URL con el parámetro tsid
-        generatedUrl = `https://tssrvy.com/r/?st=1&tsid=${tsid}`;
-      } else {
-        throw new Error('URL no válida');
-      }
-
-      return generatedUrl;
-    } catch (e) {
-      return null;
+    // Verificar si la URL es de decipherinc.com
+    if (domain.includes('decipherinc.com') && tsid) {
+      // Generar la nueva URL con el parámetro tsid
+      generatedUrl = `https://tssrvy.com/r/?st=1&tsid=${tsid}`;
+    } else {
+      throw new Error('URL no válida');
     }
+
+    return generatedUrl;
+  } catch (e) {
+    return null;
   }
 }
+
+
+
+
+
+
+
 
 // Manejo del formulario y visualización de la URL generada
 document.getElementById('urlForm').addEventListener('submit', function (event) {
