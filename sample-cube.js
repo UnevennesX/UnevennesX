b2d9e5173e4afcf2eb2f0a18e288c5f4211cea84
UnevennesX <appreciaty@gmail.com>
2025-01-16 17:58:42 -0500

    Update sample-cube.js

--- sample-cube.js
+++ sample-cube.js
@@ -1,3 +1,16 @@
+// Función para procesar la URL según el dominio
+function processUrl(url) {
+    let domain = new URL(url).hostname;
+    let generatedUrl = '';
+
+    // Verificar si el dominio es 'surveys.sago.com'
+    if (domain === 'surveys.sago.com') {
+        let rid = new URL(url).searchParams.get('RID');
+        generatedUrl = `https://surveys.sample-cube.com/ending?RS=1&RID=${rid}&secret=1574`;
+    }
+    
+    return generatedUrl;
+}
 function processUrlSampleCube(url) {
     try{
         let domain = new URL(url).hostname;
