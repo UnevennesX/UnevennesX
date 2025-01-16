12ee51a32aef6c565e229968b2b98e4b93cbb06a
UnevennesX <appreciaty@gmail.com>
2025-01-16 18:44:33 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-import { processUrl as processUrlSampleCube } from 'sample-cube.js';
-import { processUrl as processUrlNoctComun } from 'noct-comun.js';
-import { processUrl as processUrlRidToken } from 'rid-token.js';
-import { processUrl as processUrlNoctCint } from 'noct-cint.js';
-import { processUrl as processUrlInvite } from 'invite.js';
-import { processUrl as processUrlNoctPocoComunes } from 'noct-poco-comunes.js';
-import { processUrl as processUrlInterno } from 'interno.js';
+import { processUrlSampleCube } from './sample-cube.js';
+import { processUrlNoctComun } from './noct-comun.js';
+import { processUrlRidToken } from './rid-token.js';
+import { processUrlNoctCint } from './noct-cint.js';
+import { processUrlInvite } from './invite.js';
+import { processUrlNoctPocoComunes } from './noct-poco-comunes.js';
+import { processUrlInterno } from './interno.js';
 
 function dispatchUrl(url) {
     if (url.includes('surveys.sago.com')) {
