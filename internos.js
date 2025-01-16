71b2def620f5b9faae50815c83d205de2fa2d9c8
UnevennesX <appreciaty@gmail.com>
2025-01-16 09:35:45 -0500

    Update internos.js

--- internos.js
+++ internos.js
@@ -2,27 +2,20 @@ function processUrl(url) {
   try {
     // Verificar si la URL es válida
     const parsedUrl = new URL(url);
-    console.log('URL procesada:', parsedUrl);
-
     let domain = parsedUrl.hostname;
     let s2 = parsedUrl.searchParams.get('s2');
     let rd_proj_ud = parsedUrl.searchParams.get('rd_proj_ud');
     let rdud = parsedUrl.searchParams.get('rdud');
     let generatedUrl = '';
 
-    console.log('Dominio:', domain);
-    console.log('s2:', s2);
-    console.log('rd_proj_ud:', rd_proj_ud);
-    console.log('rdud:', rdud);
-
-    // Verificar si el dominio es de 'se.navigatorsurveys.com'
+    // Verificar si el dominio es 'se.navigatorsurveys.com'
     if (domain.includes('se.navigatorsurveys.com')) {
-      // Verificar que el parámetro rd_proj_ud esté presente
+      // Asegurarse de que rd_proj_ud esté presente
       if (!rd_proj_ud) {
         throw new Error('El parámetro "rd_proj_ud" es obligatorio.');
       }
 
-      // Generar la nueva URL con los parámetros
+      // Generar la nueva URL con los parámetros obtenidos
       if (s2 && rdud) {
         generatedUrl = `https://www.rdsecured.com/return?inbound_code=1000&rdud=${rdud}&rd_proj_ud=${rd_proj_ud}`;
       } else if (s2) {
@@ -47,10 +40,8 @@ function processUrl(url) {
 document.getElementById('urlForm').addEventListener('submit', function (event) {
   event.preventDefault();
 
-  const urlInput = document.getElementById('urlInput');
-  const result = processUrl(urlInput.value); // Llamamos a la función processUrl
-
-  console.log('Resultado de la URL generada:', result);
+  const urlInput = document.getElementById('urlInput').value;
+  const result = processUrl(urlInput); // Llamamos a la función processUrl
 
   if (result) {
     document.getElementById('generatedTitle').classList.remove('hidden');
