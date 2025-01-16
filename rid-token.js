c2440d9c33954a6502b100a5ae19db74d2cf198d
UnevennesX <appreciaty@gmail.com>
2025-01-16 18:30:07 -0500

    Update rid-token.js

--- rid-token.js
+++ rid-token.js
@@ -4,7 +4,7 @@ export async function processUrl(url) {
         let domain = new URL(url).hostname;
        const token = '034287b2-1ca0-48d1-9e45-f5ca740ef529';
       if (domain.includes('lumen-research.com')) {
-         let rid = new URL(url).searchParams.get('RID');
+         let rid = new URL(url).searchParams.get('participant');
          if(rid && token){
             return { url: `https://notch.insights.supply/cb?RID=${rid}&token=${token}`, provider: 'Lumen Research' };
           }
