e19bafa904484c6badcd739ff8081884c187f56f
UnevennesX <appreciaty@gmail.com>
2025-01-16 17:43:18 -0500

    Update noct-cint.js

--- noct-cint.js
+++ noct-cint.js
@@ -1,11 +1,11 @@
 function processUrlNoctCint(url) {
     try {
-      let domain = new URL(url).hostname;
+       let domain = new URL(url).hostname;
        const token = '0749a007-a1d3-48c1-8ff3-12960c555867';
       let arid = new URL(url).pathname.split('/')[2];
       let rid = new URL(url).searchParams.get('RID');
       if (domain.includes('router.cint.com') && arid && rid && token) {
-         return {url: `https://notch.insights.supply/cb?token=${token}&RID=${rid}&cint_arid=${arid}`, provider: 'Cint'};
+        return {url: `https://notch.insights.supply/cb?token=${token}&RID=${rid}&cint_arid=${arid}`, provider: 'Cint'};
       }
         return null;
     } catch (e) {
