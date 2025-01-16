fad83e1f631e8ec4084ebc2d7172c951d4ac14b1
UnevennesX <appreciaty@gmail.com>
2025-01-16 16:31:46 -0500

    Update unevennesx.js

--- unevennesx.js
+++ unevennesx.js
@@ -14,6 +14,9 @@ async function processUrlsFromTxt() {
             let name = null;
             let url = null;
             let provider = null;
+             let template = null;
+            let rid = null;
+
 
             for (const line of lines) {
                 const [key, value] = line.split(":").map(item => item.trim());
@@ -22,18 +25,41 @@ async function processUrlsFromTxt() {
                     name = value;
                 } else if (key === "URL") {
                     url = value;
+                   
+                     try {
+                      const urlParams = new URLSearchParams(new URL(value).search);
+                         rid = urlParams.get('RID');
+                      } catch(error){
+                          rid = null
+                      }
+                   
                 } else if (key === "Tipo de Survey"){
                      provider = value;
-                }
+                } else if (key === 'Template'){
+                    template = value;
+                  }
             }
             
-             if (name && url) {
-               const processed = dispatchUrl(url);
-                if(processed && processed.url){
-                  results.push({ name, url: processed.url, provider: processed.provider});
-                } else {
-                  results.push({name, url: 'URL no valida', provider: 'No se pudo procesar'});
-              }
+            if (name && url) {
+                 let finalUrl = url;
+                   if (rid === null || rid === undefined || rid === ''){
+                    finalUrl = url
+                   } else if(rid.startsWith('67')) {
+                   finalUrl = url;
+                   }else if (template){
+                        const urlWithoutRid = url.split('&RID=')[0];
+                        finalUrl = `${urlWithoutRid}&RID=67`;
+
+                   }
+                   
+                     const processed = dispatchUrl(finalUrl);
+                     if(processed && processed.url){
+                        results.push({ name, url: processed.url, provider: processed.provider});
+                      } else if (template) {
+                         results.push({ name, url: `Sugerencia: ${template}`, provider: 'No se pudo procesar'});
+                      } else {
+                           results.push({name, url: 'URL no valida', provider: 'No se pudo procesar'});
+                      }
             }
         }
         return results;
