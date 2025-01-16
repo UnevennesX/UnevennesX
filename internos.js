e2d338c5258b11d6ce0aec6d1199eb0a5318d810
UnevennesX <appreciaty@gmail.com>
2025-01-16 08:58:40 -0500

    Update internos.js

--- internos.js
+++ internos.js
@@ -1,4 +1,4 @@
-function processUrl(url) {
+function processNavigatorsUrl(url) {
   try {
     // Crear un objeto URL para analizar la URL dada
     const parsedUrl = new URL(url);
@@ -44,8 +44,33 @@ function processUrl(url) {
   }
 }
 
+// Manejo del formulario y visualización de la URL generada
+document.getElementById('urlForm').addEventListener('submit', function (event) {
+  event.preventDefault();
 
+  const urlInput = document.getElementById('urlInput');
+  const result = processNavigatorsUrl(urlInput.value);  // Usamos la nueva función
 
+  // Mostrar la URL generada o error según corresponda
+  if (result) {
+    document.getElementById('generatedTitle').classList.remove('hidden');
+    document.getElementById('generatedUrl').classList.remove('hidden');
+    document.getElementById('generatedUrl').innerText = result;
+    document.getElementById('error').classList.add('hidden');
+  } else {
+    document.getElementById('generatedTitle').classList.add('hidden');
+    document.getElementById('generatedUrl').classList.add('hidden');
+    document.getElementById('error').classList.remove('hidden');
+  }
+});
 
-
-
+// Copiar la URL generada al portapapeles
+document.getElementById('generatedUrl').addEventListener('click', function() {
+  var el = document.createElement('textarea');
+  el.value = this.innerText;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+  document.getElementById('notification').classList.remove('hidden');
+});
