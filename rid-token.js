9103ad7989bc8f92b728f83f4907e53cfd88d7fc
UnevennesX <appreciaty@gmail.com>
2025-01-16 18:27:26 -0500

    Update rid-token.js

--- rid-token.js
+++ rid-token.js
@@ -1,13 +1,16 @@
-
-function processUrl(url) {
-  let domain = new URL(url).hostname;
-  let generatedUrl = '';
-
-  if (domain.includes('lumen-research.com')) {
-    let token = '034287b2-1ca0-48d1-9e45-f5ca740ef529'; // Token de Lumen
-    let rid = new URL(url).searchParams.get('RID'); // Obtener el parámetro 'RID'
-    generatedUrl = `https://notch.insights.supply/cb?RID=${rid}&token=${token}`; // URL generada
+// rid-token.js
+export function processUrl(url) {
+    try{
+        let domain = new URL(url).hostname;
+       const token = '034287b2-1ca0-48d1-9e45-f5ca740ef529';
+      if (domain.includes('lumen-research.com')) {
+         let rid = new URL(url).searchParams.get('RID');
+         if(rid && token){
+            return { url: `https://notch.insights.supply/cb?RID=${rid}&token=${token}`, provider: 'Lumen Research' };
+          }
+        }
+     return null;
+     } catch(e){
+          return null
+    }
   }
-
-  return generatedUrl;
-}
