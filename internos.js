368162e7638beae807b97229909cabe7d37a6367
UnevennesX <appreciaty@gmail.com>
2025-01-16 09:58:17 -0500

    Update internos.js

--- internos.js
+++ internos.js
@@ -1,60 +1,19 @@
 function processUrl(url) {
-  try {
-    let domain = new URL(url).hostname;
-    let s2 = new URL(url).searchParams.get('s2');
-    let rd_proj_ud = new URL(url).searchParams.get('rd_proj_ud');
-    let rdud = new URL(url).searchParams.get('rdud');
-    let generatedUrl = '';
+          // Expresión regular para extraer el tsid
+          const regex = /tsid=([a-f0-9]{32})/;
+          const match = url.match(regex);
 
-    // Verificar si el dominio es el esperado
-    if (domain.includes('navigatorsurveys.com')) {
-      // Generar la nueva URL con los parámetros obtenidos
-      if (rd_proj_ud) {
-        generatedUrl = `https://www.rdsecured.com/return?inbound_code=1000&rd_proj_ud=${rd_proj_ud}`;
-        if (rdud) generatedUrl += `&rdud=${rdud}`;
-        if (s2) generatedUrl += `&s2=${s2}`;
-      } else {
-        throw new Error('El parámetro "rd_proj_ud" es obligatorio.');
+          if (match) {
+              const tsid = match[1]; // El tsid extraído de la URL
+              return `https://tssrvy.com/r/?st=1&tsid=${tsid}`; // Generar el nuevo enlace
+          }
+          return null; // Si no se encuentra el tsid, retornar null
       }
-    } else {
-      throw new Error('URL no válida');
-    }
 
-    return generatedUrl;
-  } catch (e) {
-    console.error('Error:', e.message); // Log de error para depuración
-    return null;
-  }
-}
+      function toggleList() {
+          const domains = document.querySelector('.allowed-domains');
+          domains.classList.toggle('open');
+      }
 
-document.getElementById('urlForm').addEventListener('submit', function (event) {
-  event.preventDefault(); // Evitar recarga de la página
 
-  const urlInput = document.getElementById('urlInput').value;
-  const result = processUrl(urlInput);
 
-  if (result) {
-    // Mostrar la URL generada
-    document.getElementById('generatedTitle').classList.remove('hidden');
-    document.getElementById('generatedUrl').classList.remove('hidden');
-    document.getElementById('generatedUrl').innerText = result;
-    document.getElementById('error').classList.add('hidden');
-  } else {
-    // Mostrar el mensaje de error
-    document.getElementById('generatedTitle').classList.add('hidden');
-    document.getElementById('generatedUrl').classList.add('hidden');
-    document.getElementById('error').classList.remove('hidden');
-  }
-});
-
-// Copiar la URL generada al portapapeles
-document.getElementById('generatedUrl').addEventListener('click', function() {
-  const el = document.createElement('textarea');
-  el.value = this.innerText;
-  document.body.appendChild(el);
-  el.select();
-  document.execCommand('copy');
-  document.body.removeChild(el);
-
-  document.getElementById('notification').classList.remove('hidden');
-});
