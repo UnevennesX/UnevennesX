7a0c3c257c31f7293fd3846d18c1bf33dcf263d7
UnevennesX <appreciaty@gmail.com>
2025-01-16 19:26:16 -0500

    Update rid-token.js

--- rid-token.js
+++ rid-token.js
@@ -3,19 +3,22 @@
 function processUrl(url) {
   try {
     let domain = new URL(url).hostname;
-    const token = '034287b2-1ca0-48d1-9e45-f5ca740ef529';
+    let generatedUrl = '';
 
     if (domain.includes('lumen-research.com')) {
-      let participant = new URL(url).searchParams.get('participant');
-      if (participant) {  // Verificar que 'participant' exista antes de usarlo
-        return `https://notch.insights.supply/cb?RID=${participant}&token=${token}`;
-      }
-      return 'URL no válida'; // Si no hay 'participant', retorna 'URL no válida'
-    }
+      let token = '034287b2-1ca0-48d1-9e45-f5ca740ef529';
+      let rid = new URL(url).searchParams.get('RID');
 
-    return null; // Si no es un dominio de lumen-research
-  } catch (e) {
-    return null; // Si ocurre un error al parsear la URL
+      if (rid) { // Verifica que 'rid' no sea nulo o indefinido
+        generatedUrl = `https://notch.insights.supply/cb?RID=${rid}&token=${token}`;
+      } else {
+         return 'URL no válida'; // Retorna 'URL no válida' si no hay RID
+      }
+    }
+     return generatedUrl; // Retorna la URL generada si se pudo crear o "" si no se cumplio el if principal
+  } catch (error) {
+      console.error('Error al procesar la URL:', error);
+    return null; // Retorna null si ocurre un error al parsear
   }
 }
 
