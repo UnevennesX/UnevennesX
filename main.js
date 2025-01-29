46ea19b4f6e4ea01fccb12b845ab94fa2fa805b3
UnevennesX <appreciaty@gmail.com>
2025-01-29 11:32:37 -0900

    Update main.js

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ function processUrlSampleCube(url) {
     let generatedUrl = '';
     if (domain === 'surveys.sago.com') {
         let rid = new URL(url).searchParams.get('RID');
-        generatedUrl = `https://surveys.sample-cube.com/ending?RS=1&RID=${rid}&secret=1574`;
+        generatedUrl = `https://surveys.sample-cube.com/ending?RS=1&RID=${rid}&secret=3112`;
     }
     return generatedUrl;
 }
